60613bdfcbdc3ef778cc4f228c1d50cf151d0997
Aslan <info@debishev.net>
2024-11-11 00:00:52 +0300

    Fixed version

--- src/DependencyInjection/Bitrix24LibraryExtension.php
+++ src/DependencyInjection/Bitrix24LibraryExtension.php
@@ -10,11 +10,14 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 class Bitrix24LibraryExtension extends Extension
 {
 
+    /**
+     * @throws \Exception
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader(
             $container,
-            new FileLocator(__DIR__ . '/../../config')
+            new FileLocator(__DIR__ . '/../config')
         );
 
         $loader->load('services.yaml');
