eb7c503f95115649f82c458c2b6ab716435d22fe
Aslan <info@debishev.net>
2024-11-10 23:57:40 +0300

    Fixed version

--- src/Bitrix24LibraryBundle.php
+++ src/Bitrix24LibraryBundle.php
@@ -2,8 +2,10 @@
 
 namespace Debishev\Bitrix24Library;
 
+use Debishev\Bitrix24Library\DependencyInjection\Bitrix24LibraryExtension;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
@@ -36,6 +38,14 @@ class Bitrix24LibraryBundle extends AbstractBundle
 //            ->arg(0, $config['bitrix24']['webhook_url'])
 //            ->arg(1, $config['bitrix24']['base_url'])
 //        ;
+
+
+
+    }
+
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new Bitrix24LibraryExtension();
     }
 
 
